e1739a42ba52d7660d9e1c2813cca1fa773e92c9
Nick Hodges <nickhodges@gmail.com>
2019-03-24 10:52:53 -0400

    Getting basic material tabs to work with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 import { Tab1Component } from './tab1/tab1.component';
 import { Tab2Component } from './tab2/tab2.component';
 import { Tab3Component } from './tab3/tab3.component';
+import { MatTabsModule } from '@angular/material';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, Tab1Component, Tab2Component, Tab3Component],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTabsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
