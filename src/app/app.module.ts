384a8def6d1346ee7834be9287ec4cd931faa09a
Nick Hodges <nickhodges@gmail.com>
2019-03-24 10:33:25 -0400

    Adding three components, one for each tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Tab1Component } from './tab1/tab1.component';
+import { Tab2Component } from './tab2/tab2.component';
+import { Tab3Component } from './tab3/tab3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Tab1Component,
+    Tab2Component,
+    Tab3Component
   ],
   imports: [
     BrowserModule
