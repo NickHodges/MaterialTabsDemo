15c951ee87e50d8b6fb23dd9ee9deca230ef5ddc
Nick Hodges <nickhodges@gmail.com>
2019-03-24 10:42:51 -0400

    Add Material stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,12 @@ import { Tab1Component } from './tab1/tab1.component';
 import { Tab2Component } from './tab2/tab2.component';
 import { Tab3Component } from './tab3/tab3.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    Tab1Component,
-    Tab2Component,
-    Tab3Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Tab1Component, Tab2Component, Tab3Component],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
